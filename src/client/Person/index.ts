0076c2c53f32e6387e5b142ddca6984165a625fa
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2019-04-06 15:13:12 -0300

    adiciona get de pessoas

--- src/client/Person/index.ts
+++ src/client/Person/index.ts
@@ -1,12 +1,25 @@
 import Person from "../../models/Person";
+import Filters from "../utils/Filters";
+import api from "../../services/api";
 
 export default class PersonClient {
-  private readonly token: string;
+  private readonly _token: string;
+  private readonly _prefix: string;
+
   constructor(token: string) {
-    this.token = token;
+    this._token = token;
+    this._prefix = "/persons";
   }
 
-  get(query: object): Promise<Person> {
-    return new Promise((resolve, reject) => resolve())
-  } 
+  get(query?: Filters): Promise<Person[]> {
+    const response = api
+      .get(this._prefix, {
+        params: {
+          token: this._token
+        }
+      })
+      .then(res => res.data);
+
+    return response;
+  }
 }
